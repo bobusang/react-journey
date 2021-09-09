73353ce7796d8308387ba29f06d0590263437832
Hamzah Ramadhan <hamzahram10@gmail.com>
2021-09-09 22:32:00 +0700

    finalize installing Tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
+            <div className="
+            bg-green-200
+            flex
+            items-center
+            justify-center
+            ">
+
             <h1> Hey this is the Navbar!</h1>
+            </div>
         </div>
     )
 }
