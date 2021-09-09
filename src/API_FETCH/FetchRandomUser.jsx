06476b6ce2096270b99e1de7af8367cd6f263219
Hamzah Ramadhan <hamzahram10@gmail.com>
2021-09-10 00:15:41 +0700

    Successfully fetch API from random user me using useEffect

--- src/API_FETCH/FetchRandomUser.jsx
+++ src/API_FETCH/FetchRandomUser.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useEffect, useState, setState } from "react";
 
 function FetchRandomUser() {
+    const [loading, setLoading] = useState(true);
+    const [person, setPerson] = useState(null)
+
+     useEffect (() => {
+         (async () => {
+             const url = "https://api.randomuser.me/";
+             const response = await fetch(url);
+             const data = await response.json();
+            setPerson(data.results[0]);
+            setLoading(false);
+            console.log(person)
+
+         }) ()
+    }, []) 
+
     return (
-        <div>
-            Hey this is API Page
+        <div className="pt-20 grid place-items-center">
+            {loading || !person ? (
+            <div className="">
+                loading...
+            </div>):(
+            <div>
+                <img src={person.picture.large} />
+                
+                {person.name.first}
+            </div>
+            )}
         </div>
     )
 }
