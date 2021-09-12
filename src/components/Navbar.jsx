96baa48b408c3d7435012daa6e1ec5138f2d899e
Hamzah Ramadhan <hamzahram10@gmail.com>
2021-09-12 09:35:59 +0700

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,7 @@ function Navbar() {
             items-center
             justify-between
             py-4
+            px-4
             mx-auto
             container
             ">
@@ -28,7 +29,8 @@ function Navbar() {
                 </Link>
 
                 <div className="
-                flex
+                hidden
+                sm:flex
                 items-center
                 justify-center
                 space-x-6
@@ -42,8 +44,19 @@ function Navbar() {
                     <div className="text-xl font-semibold text-white py-2 px-4 duration-75 hover:bg-indigo-400 rounded-2xl"> Project 4</div>
                 </div>
 
-                <div className="text-xl font-semibold text-white py-2 px-4 bg-blue-400 rounded-2xl"> Contact me!
+                <div className="hidden sm:block text-xl font-semibold text-white py-2 px-4 bg-blue-400 rounded-2xl"> Contact me!</div>
 
+                <div className="group sm:hidden text-xl font-semibold text-white py-2 px-4 border-2 border-green-600 hover:bg-green-600 hover:border-white rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 group-hover:text-white text-green-600" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+
+                
+            </div>
+            <div className="flex items-center justify-center">
+                 <div className="absolute bottom-10 sm:hidden text-xl font-semibold text-white py-2 px-4 bg-blue-400 rounded-full shadow-lg">
+                    Contact me!
                 </div>
             </div>
         </div>
